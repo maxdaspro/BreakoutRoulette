014c7fbbfff931b711254c7d50da11b96e49864d
bernard <janss.bernard@gmail.com>
2017-05-21 16:23:29 +0200

    corrections

--- script/class/Roulette.js
+++ script/class/Roulette.js
@@ -4,9 +4,9 @@ class Roulette{
 
         this.position = position;
 
-        this.center = Helper.Phaser.drawPoint(position, 0xA6AAB0, 150);
-        this.center.alpha = 0.2;
+        this.center = Helper.Phaser.drawPoint(position, 0xA6AAB0, 160);
+        this.center.alpha = 0;
 
-        Helper.Phaser.drawPoint(position, 0xFF0000, 1);
+        // Helper.Phaser.drawPoint(position, 0xFF0000, 1);
     }
 }
