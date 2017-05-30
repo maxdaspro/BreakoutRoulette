6120971da855b3ce3f033dba7bc743393c582b91
bernard <janss.bernard@gmail.com>
2017-05-30 09:41:33 +0200

    sounds

--- script/class/Roulette.js
+++ script/class/Roulette.js
@@ -21,7 +21,7 @@ class Roulette {
         this.max = 9;
         this.stepAngle = 360 / this.amount;
 
-        this.level = 0;
+        this.level = 1;
 
         this.items = [];
 
@@ -37,9 +37,10 @@ class Roulette {
     }
 
     start() {
-
+        startsound.play();
         this.message.alert('Level ' + this.level, () => {
-            startsound.play();
+
+
 
             this.generateItems();
 
