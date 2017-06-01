424f70f06e19a52bc7a7294dae18e624bdcaa1e0
Olivier <okathenis@gmail.com>
2017-06-01 15:30:21 +0200

    sons

--- script/class/Roulette.js
+++ script/class/Roulette.js
@@ -41,7 +41,7 @@ class Roulette {
 
     start() {
 
-        startSound.volume = 0.5;
+        startSound.volume = 0.4;
         startSound.play();
 
         this.message.alert('Level ' + this.level, () => {
