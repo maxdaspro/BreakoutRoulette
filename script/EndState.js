cb232556b658ce102bc67caf91eddd45a3fa4ff9
Olivier <okathenis@gmail.com>
2017-06-06 10:48:18 +0200

    endgame

--- script/EndState.js
+++ script/EndState.js
@@ -5,12 +5,41 @@ EndState.preload = function () {
 }
 
 EndState.create = function () {
-    console.log('EndState');
+	console.log('EndState');
 
-    $('#interface #content').load('template/end.phtml', () => {
-        
-        console.log('end loaded');
-    });
+	$('#interface #content').load('template/end.phtml', () => {
+		game.input.gamepad.start();
+
+		game.input.gamepad._gamepads.forEach((gamepad, index) => {
+			gamepad.onConnectCallback = function () {
+
+
+				let playButton;
+				gamepad.getButton(Phaser.Gamepad.XBOX360_DPAD_UP).onDown.add(function () {
+					if (playButton = document.getElementById("change")) {playButton.classList.remove('ready');}
+						
+					playButton = document.getElementById("restart");
+					console.log('up');
+					playButton.classList.add('ready');
+				});
+				gamepad.getButton(Phaser.Gamepad.XBOX360_DPAD_DOWN).onDown.add(function () {
+					if (playButton = document.getElementById("restart")) {playButton.classList.remove('ready');}
+					playButton = document.getElementById("change");
+					console.log('down');
+					playButton.classList.add('ready');
+				});
+				gamepad.getButton(Phaser.Gamepad.BUTTON_9).onDown.add(function () {
+					launchGame();
+				});
+
+			}
+		});
+
+		function launchGame() {
+			document.getElementById("interface").classList.add('hidden');
+			game.state.start('play');
+		}
+	});
 }
 
 EndState.update = function () {
